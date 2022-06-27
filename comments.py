f8c59775dc10c621cc7cfcededb75333d8c72e03
Hemanth Krishna Nanda <107603676+hemanth365@users.noreply.github.com>
2022-06-27 18:36:42 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+"""print('hello world')
+print("hello world")
+print('''hello world''')
+print('hello wo\
+rld')"""
+
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print('hello wo\
+# rld')
